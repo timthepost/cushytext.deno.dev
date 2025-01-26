780062754ff1958a31a7cbf5b8043354557f1048
= <=>
2025-01-26 13:17:10 -0500

    infima: load via site.remoteFIle()

--- _config.ts
+++ _config.ts
@@ -5,6 +5,15 @@ const site = lume({
   src: "./src",
 });
 
+site.remoteFile(
+  "_includes/css/infima-light.css",
+  "https://cdn.jsdelivr.net/npm/infima@0.2.0-alpha.45/dist/css/default/default.css",
+);
+site.remoteFile(
+  "_includes/css/infima-dark.css",
+  "https://cdn.jsdelivr.net/npm/infima@0.2.0-alpha.45/dist/css/default-dark/default-dark.css",
+);
+
 site.use(plugins());
 
 export default site;
--- src/_includes/js/main.js
+++ src/_includes/js/main.js
@@ -18,7 +18,7 @@ function sync() {
   const userTheme = getLocalStorageOrDefault("theme", "light");
 
   function syncCSS() {
-    themeStyleSheet.href = `/infima-css/theme-${userTheme}.css`;
+    themeStyleSheet.href = `/infima-css/infima-${userTheme}.css`;
   }
 
   function syncTheme() {
