b359b2b85ce2820d8f87e5dae1b000df4eb01a63
= <=>
2025-02-17 17:19:09 -0500

    Upgrade to v3 dev. BREAKING.

--- plugins.ts
+++ plugins.ts
@@ -1,13 +1,11 @@
-import lightningcss from "lume/plugins/lightningcss.ts";
+import postcss from "lume/plugins/postcss.ts";
 import basePath from "lume/plugins/base_path.ts";
 import metas from "lume/plugins/metas.ts";
 import { Options as SitemapOptions, sitemap } from "lume/plugins/sitemap.ts";
 import { favicon, Options as FaviconOptions } from "lume/plugins/favicon.ts";
 import { merge } from "lume/core/utils/object.ts";
-import jsx from "lume/plugins/jsx_preact.ts";
 import mdx from "lume/plugins/mdx.ts";
 import toc from "https://deno.land/x/lume_markdown_plugins@v0.7.1/toc.ts";
-import pagefind from "lume/plugins/pagefind.ts";
 
 import "lume/types.ts";
 
@@ -27,27 +25,19 @@ export default function (userOptions?: Options) {
   const options = merge(defaults, userOptions);
 
   return (site: Lume.Site) => {
-    site.use(lightningcss())
-      .use(pagefind({
-        ui: {
-          containerId: "search",
-          showImages: false,
-          showEmptyFilters: true,
-          resetStyles: true,
-        },
-      }))
+    site.use(postcss())
+      .add(".css")
       .use(basePath())
       .use(toc())
       .use(metas())
-      .use(jsx())
       .use(mdx({ extensions: [".mdx"] }))
       .use(sitemap(options.sitemap))
       .use(favicon(options.favicon))
-      .copy("uploads")
-      .copy("serve.ts")
-      .copy("_includes/js", "js")
-      .copy("_includes/css", "infima-css");
-
+      .add("uploads")
+      .add("serve.ts")
+      .add("main.js", ".")
+      .add("_includes/js", "js")
+      .add("_includes/css", "infima-css");
     /*
       site.remoteFile(
           "_includes/css/infima-light.css",
