b9df172cc5baa2f30c7edd614f425f84742e1192
= <=>
2025-02-24 16:07:06 -0500

    Generate a link map of the site in JSON. This avoids helpers having to include lume.

--- src/menu.page.ts
+++ src/menu.page.ts
@@ -1,16 +1,14 @@
-
-
 export default function* ({ nav }: Lume.Data) {
-    const menu = nav.menu(
-      "/",
-      `hide_menu!=true`,
-      "order=asc title=asc-locale basename=asc-locale",
-    )?.children ||
-      [];
+  const menu = nav.menu(
+    "/",
+    `hide_menu!=true`,
+    "order=asc title=asc-locale basename=asc-locale",
+  )?.children ||
+    [];
 
-    yield {
-      url: `/menu.json`,
-      content: JSON.stringify(menu, null, 2),
-    };
-    return;
+  yield {
+    url: `/menu.json`,
+    content: JSON.stringify(menu, null, 2),
+  };
+  return;
 }
