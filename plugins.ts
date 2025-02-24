e8c40f54a985f7db96dfc266c441deaaf6bdf517
= <=>
2025-02-24 08:05:26 -0500

    plugins: cleanup & organize imports

--- plugins.ts
+++ plugins.ts
@@ -1,9 +1,9 @@
-import postcss from "lume/plugins/postcss.ts";
-import basePath from "lume/plugins/base_path.ts";
-import metas from "lume/plugins/metas.ts";
 import { Options as SitemapOptions, sitemap } from "lume/plugins/sitemap.ts";
 import { favicon, Options as FaviconOptions } from "lume/plugins/favicon.ts";
 import { merge } from "lume/core/utils/object.ts";
+import postcss from "lume/plugins/postcss.ts";
+import basePath from "lume/plugins/base_path.ts";
+import metas from "lume/plugins/metas.ts";
 import mdx from "lume/plugins/mdx.ts";
 import toc from "https://deno.land/x/lume_markdown_plugins@v0.7.1/toc.ts";
 
@@ -33,14 +33,6 @@ export default function (userOptions?: Options) {
       .use(mdx({ extensions: [".mdx"] }))
       .use(sitemap(options.sitemap))
       .use(favicon(options.favicon))
-      .remoteFile(
-        "_includes/css/theme-light.css",
-        "https://cdn.jsdelivr.net/npm/infima@0.2.0-alpha.45/dist/css/default/default.css",
-      )
-      .remoteFile(
-        "_includes/css/theme-dark.css",
-        "https://cdn.jsdelivr.net/npm/infima@0.2.0-alpha.45/dist/css/default-dark/default-dark.css",
-      )
       .add("_includes/js", "js")
       .add("_includes/css", "css")
       .add("uploads")
