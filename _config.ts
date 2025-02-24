f4f105026e786b51fd38843188e4db2790132b21
= <=>
2025-02-24 08:04:59 -0500

    _config: import router middleware

--- _config.ts
+++ _config.ts
@@ -1,8 +1,13 @@
 import lume from "lume/mod.ts";
 import plugins from "./plugins.ts";
+import router from "./src/server_routes.ts";
 
 const site = lume({
   src: "./src",
+  location: new URL("https://example.deno.dev"),
+  server: {
+    middlewares: [router.middleware()],
+  },
 });
 
 site.use(plugins());
