514f43ecdf7329e6402de90fb2300c1ed9e13d32
= <=>
2025-02-25 15:54:05 -0500

    Explain this page

--- src/menu.page.ts
+++ src/menu.page.ts
@@ -1,3 +1,20 @@
+/**
+ * Write a navigation-tree-walk so server Middleware
+ * can easily access the output of /nav.
+ *
+ * This gets processedd like any other content page, 
+ * and is NOT guaranteed to run in-time for templates
+ * to use it. 
+ * 
+ * This is intended to be consumed *after* the server has 
+ * started.
+ *
+ * See the /nav code in _server_routes.ts
+ *  
+ * @nav Lume Nav() (from the plugin)
+ * @writes _site/menu.json 
+ */
+
 export default function* ({ nav }: Lume.Data) {
   const menu = nav.menu(
     "/",
@@ -5,7 +22,6 @@ export default function* ({ nav }: Lume.Data) {
     "order=asc title=asc-locale basename=asc-locale",
   )?.children ||
     [];
-
   yield {
     url: `/menu.json`,
     content: JSON.stringify(menu, null, 2),
