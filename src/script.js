0b3d7acb2bdc29b6b4b6e7f0ce0585c88839e38a
= <=>
2025-03-11 10:26:55 -0400

    Be specific about purpose of script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-// Here For The Plugins
+/**
+ * Here for site customization, plugins that need to write things,
+ * or whatever else is needed to make things work. This is loaded
+ * last by the template, and is here so you don't have to edit the 
+ * template scripts directly. 
+ */
