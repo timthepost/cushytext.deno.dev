3e36775aa9632de4bda9074e55ebd2a5807ffe80
= <=>
2025-03-19 10:43:31 -0400

    No longer break the local server, use beforeBuild and only run once.

--- src/_plugins/site_conductor/mod.ts
+++ src/_plugins/site_conductor/mod.ts
@@ -87,7 +87,7 @@ export default function conductor(userOptions?: Options) {
     });
 
     
-    site.addEventListener("afterRender", async() => {
+    site.addEventListener("beforeBuild", async() => {
       const tagWiki : TagWikiData = await yaml(options.tag_wiki_path);
       for (const tagUnknown of site.search.values("tags")) {
         const tag = tagUnknown as string;
