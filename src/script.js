3a9b8e818221c62e452d6cd8ebd941530a7ee1c5
= <=>
2025-03-21 21:13:03 -0400

    No need for default value in script.js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,1 @@
-/**
- * Here for site customization, plugins that need to write things,
- * or whatever else is needed to make things work. This is loaded
- * last by the template, and is here so you don't have to edit the
- * template scripts directly.
- */
-export const _script = "/script.js";
+
