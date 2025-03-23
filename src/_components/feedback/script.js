4e11ede5ffb19301f66531fb2136bddd8e7f4fac
= <=>
2025-03-23 18:05:21 -0400

    comp.feedback: simplify strings for localization (no strings in the JS)

--- src/_components/feedback/script.js
+++ src/_components/feedback/script.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   const voteButtons = document.querySelectorAll(".page-feedback-vote");
   const header = document.getElementById("page-feedback-header");
-  const label = document.getElementById("page-feedback-label");
   const comment = document.getElementById("page-feedback-comment");
   const clear = document.getElementById("page-feedback-clear");
   const submit = document.getElementById("page-feedback-submit");
@@ -21,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function updateCharCount() {
     const remainingChars = MAX_CHARS - comment.value.length;
-    charCountDisplay.textContent = `Characters Remaining: ${remainingChars}`;
+    charCountDisplay.textContent = `${remainingChars}`;
 
     if (remainingChars < 0) {
       submit.disabled = true;
@@ -46,7 +45,6 @@ document.addEventListener("DOMContentLoaded", function () {
   clear.addEventListener("click", function (event) {
     event.preventDefault();
     comment.value = "";
-    label.textContent = "Was reading this article a good use of your time?";
     voteButtons.forEach((button) => {
       button.classList.remove(currentVoteClass);
       button.classList.add(otherVoteClass);
@@ -66,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
   voteButtons.forEach((button) => {
     button.addEventListener("click", function (event) {
       event.preventDefault();
-      const vote = parseInt(button.dataset.vote);
+      const _vote = parseInt(button.dataset.vote);
       form.style.display = "block";
       header.style.display = "none";
 
@@ -83,17 +81,6 @@ document.addEventListener("DOMContentLoaded", function () {
       submit.classList.remove(submitDisabledClass);
       submit.classList.add(submitEnabledClass);
 
-      switch (vote) {
-        case 0:
-        case -1:
-          label.textContent = "Sorry! What could be better about this page?";
-          break;
-        case 1:
-          label.textContent = "Great! What could make this page even better?";
-          break;
-        default:
-          break;
-      }
       comment.focus();
       updateCharCount();
     });
