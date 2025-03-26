e24ac7f2e03e4cde44195b12c9fc1c612521b484
= <=>
2025-03-26 08:27:20 -0400

    Secure middleware handler for feedback

--- src/_server_routes.ts
+++ src/_server_routes.ts
@@ -1,22 +1,64 @@
 import Router from "lume/middlewares/router.ts";
 
+/*
+ * We still have to filter out XSS attempts because submitted stuff could 
+ * be read in clients that are vulnerable to them.
+ */
+function sanitizeString(str: string): string {
+  if (!str) {
+    return "";
+  }
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
 const router = new Router();
 
+// The datetime server
 router.get("/api", ({ _request }) => {
   const ts = Date.now();
-  return new Response(JSON.stringify({ ping: ts }), { status: 200 });
+  return new Response(JSON.stringify({ time: ts }), { status: 200 });
 });
 
-// Add more (get / post / etc) here.
+// Handle anon content feedback form
 router.post("/api/feedback", async ({ request }) => {
   const payload = await (request.blob());
   const data = await payload.text();
   const obj = JSON.parse(data);
   const kv = await Deno.openKv();
+
+  if (! obj.basename || obj.basename.length === 0) {
+    return new Response(JSON.stringify(
+      ["error:", "missing basename"]), {
+      status: 500 
+    });
+  }
+
+  if (obj.message && obj.message.length > 650) {
+    return new Response(JSON.stringify(
+      ["error:", "message exceeds 650 characters"]), {
+      status: 500 
+    });
+  }
+
+  if (obj.vote === undefined || obj.vote > 1 || obj.vote  < -1) {
+    return new Response(JSON.stringify(
+      ["error:", "Missing vote or invalid integer (-1, 0 or 1 supported)"]), {
+      status: 500 
+    });
+  }
+
+  obj.message = sanitizeString(obj.message);
+  
   const res = await kv.set(
-    ["anonFeedback", obj.basename, Date.now(), crypto.randomUUID()],
-    data,
+    ["anonFeedback", obj.basename, crypto.randomUUID()],
+    JSON.stringify(obj),
   );
+
   if (res.ok) {
     return new Response(JSON.stringify(data), { status: 201 });
   } else {
