a6efcf590e44e3963c2d5362ebb4a921fab55c21
= <=>
2025-03-27 11:05:01 -0400

    chore: use precompress()

--- _config.ts
+++ _config.ts
@@ -1,13 +1,18 @@
 import cacheBusting from "lume/middlewares/cache_busting.ts";
 import lume from "lume/mod.ts";
 import plugins from "./plugins.ts";
+import precompress from "lume/middlewares/precompress.ts";
 import router from "./src/_server_routes.ts";
 
 const site = lume({
   src: "./src",
   location: new URL("https://cushytext.deno.dev"),
   server: {
-    middlewares: [router.middleware(), cacheBusting()],
+    middlewares: [
+      router.middleware(), 
+      cacheBusting(),
+      precompress()
+    ],
   },
 });
 
--- src/_serve.ts
+++ src/_serve.ts
@@ -2,6 +2,7 @@ import Server from "lume/core/server.ts";
 import cacheBusting from "lume/middlewares/cache_busting.ts";
 import expires from "lume/middlewares/expires.ts";
 import notFound from "lume/middlewares/not_found.ts";
+import precompress from "lume/middlewares/precompress.ts";
 import router from "./_server_routes.ts";
 
 const server = new Server({
@@ -12,6 +13,7 @@ const server = new Server({
 server.use(router.middleware());
 server.use(expires());
 server.use(cacheBusting());
+server.use(precompress());
 server.use(notFound({
   root: `${Deno.cwd()}/_site`,
   page404: "/404.html",
