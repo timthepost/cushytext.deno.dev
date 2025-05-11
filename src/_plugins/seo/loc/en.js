347dafdb533f916a94ccde42f2cb6eeb51b85079
= <=>
2025-05-11 17:21:54 -0400

    plugins: seo: lint

--- src/_plugins/seo/loc/en.js
+++ src/_plugins/seo/loc/en.js
@@ -3,11 +3,13 @@
  * Access strings via friendly static property names.
  *
  * Example:
- * import { LogMessages } from './logMessages.js'; // Adjust path as needed
+ * import { LogMessages } from "./loc/en.js";
  * console.log(LogMessages.STARTUP_MESSAGE);
  */
 export class LogMessages {
     static APP_NAME = "SimpleSEO";
 
     static STARTUP_MESSAGE = `${this.APP_NAME} is starting up...`;
-  }
+}
+
+export default { LogMessages };
